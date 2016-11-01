6bfabef9c6718bea38d39b0839b635aff13acbdd
alexbernardino <alex@isr.ist.utl.pt>
2016-11-01 14:08:05 -0700

    square

---
+++ main_square.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <square.h>
+
+void print_square(double x)
+{
+   cout << "The square of " << x << "is ";
+   cout << square(x) << endl;
+}
+
+int main()
+{
+   print_square(1.234);
+}
